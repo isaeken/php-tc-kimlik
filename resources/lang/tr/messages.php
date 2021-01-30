f000caf4a53a57d484ddfd26e9bdef051229e9a0
Isa Eken <hello@isaeken.com.tr>
2021-01-31 02:58:26 +0300

    added turkish translations

---
+++ resources/lang/tr/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "invalid_identity" => "Hatalı kimlik bilgileri",
+    "invalid_identity_number" => "Hatalı kimlik numarası",
+    "invalid_name" => "Lütfen gerçek bir isim girin",
+    "invalid_year" => "Lütfen gerçek bir yıl girin",
+];
