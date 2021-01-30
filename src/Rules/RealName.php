7e4edb2188da0f5d72b467192f708766fa77defe
Isa Eken <hello@isaeken.com.tr>
2021-01-31 01:09:26 +0300

    added RealName validator for laravel

---
+++ src/Rules/RealName.php
@@ -0,0 +1,64 @@
+<?php
+
+
+namespace IsaEken\PhpTcKimlik\Rules;
+
+
+use Illuminate\Contracts\Validation\Rule;
+use IsaEken\PhpTcKimlik\Helpers;
+
+class RealName implements Rule
+{
+    /**
+     * @var bool $required
+     */
+    protected $required;
+
+    /**
+     * @var string $attribute
+     */
+    protected $attribute;
+
+    /**
+     * IdentityNumber constructor.
+     * @param bool $required
+     */
+    public function __construct(bool $required = true)
+    {
+        $this->required = $required;
+    }
+
+    /**
+     * @return IdentityNumber
+     */
+    public function nullable() : self
+    {
+        $this->required = false;
+
+        return $this;
+    }
+
+    /**
+     * @param string $attribute
+     * @param mixed $value
+     * @return bool
+     */
+    public function passes($attribute, $value) : bool
+    {
+        $this->attribute = $attribute;
+
+        if (!is_string($value) || is_null($value)) {
+            return false;
+        }
+
+        return Helpers::verifyName($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function message() : string
+    {
+        return __("Please enter real name.");
+    }
+}
