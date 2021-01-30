4273eeb2ef77bd641a011819511c550513eb0e32
Isa Eken <hello@isaeken.com.tr>
2021-01-31 02:54:22 +0300

    added english translation

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "invalid_identity" => "Invalid identity",
+    "invalid_identity_number" => "Invalid identity number",
+    "invalid_name" => "Please enter real name",
+    "invalid_year" => "Please enter real year",
+];
